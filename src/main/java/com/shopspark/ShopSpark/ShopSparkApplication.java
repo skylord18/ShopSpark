62f03b6d9268aa1376be23246c908b9e36f91524
Bhaskar Verma <skylord1899@gmail.com>
2024-02-22 09:42:41 +0530

    Starting Project for ShopSpark Monolith

---
+++ src/main/java/com/shopspark/ShopSpark/ShopSparkApplication.java
@@ -0,0 +1,13 @@
+package com.shopspark.ShopSpark;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopSparkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShopSparkApplication.class, args);
+	}
+
+}
