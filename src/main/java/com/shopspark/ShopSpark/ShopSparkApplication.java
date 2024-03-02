166c50a7d96e14d0605914a18417f900303bf08b
Bhaskar Verma <skylord1899@gmail.com>
2024-03-02 08:38:02 +0530

    Todo Cart

--- src/main/java/com/shopspark/ShopSpark/ShopSparkApplication.java
+++ src/main/java/com/shopspark/ShopSpark/ShopSparkApplication.java
@@ -2,8 +2,10 @@ package com.shopspark.ShopSpark;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 @SpringBootApplication
+@EnableWebSecurity
 public class ShopSparkApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ShopSparkApplication.class, args);
